f7921371f8842f3f8cc82f65e57d9de7a6faad9c
x <160286903+local-x@users.noreply.github.com>
2024-12-14 08:08:14 +0700

    Add files via upload

---
+++ data/list-dir.js
@@ -0,0 +1,7 @@
+dataDir = {
+  "Chapter 0001-0100": { url: "/the-eternal-supreme/part?=1", date: "2024-12-14", size: "3.3 MB" },
+  "Chapter 0101-0200": { url: "/the-eternal-supreme/part?=2", date: "2024-12-14", size: "3.2 MB" },
+  "Chapter 0201-0300": { url: "/the-eternal-supreme/part?=3", date: "2024-12-14", size: "3.3 MB" },
+  "Chapter 0301-0400": { url: "/the-eternal-supreme/part?=4", date: "2024-12-14", size: "3.3 MB" },
+  "Chapter 0401-0500": { url: "/the-eternal-supreme/part?=5", date: "2024-12-14", size: "3.3 MB" },
+};
